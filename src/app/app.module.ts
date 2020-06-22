f75ac83ab4c701e984868dbac6196f5601c3a69a
dinob <dinob@dinobs-MacBook-Pro.local>
2020-06-22 10:49:03 -0700

    Add interceptor class, add it to app.module, and how to use it to intercept requests, modify requests by for example adding Auth header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    // Here we have to provide interceptor and it is specific in the way
+    // that it is not provided by specifying interceptor class like in 
+    // other cases we saw so far but by providing an js object with 3 keys
+    {
+      provide: HTTP_INTERCEPTORS, // what indentifier to use
+      useClass: AuthInterceptorService, // which interceptor class
+      multi: true // can we use multiple interceptors under this identifier
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
